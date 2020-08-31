1c5af9af1cbef698c9087ba6656f68d7a930911a
Sean Wei <me@sean.taipei>
2020-08-31 19:12:13 +0800

    Auto resize

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,27 @@ document.addEventListener('keyup', (e) => {
 	}
 });
 
+resize(); // init
+
 function showImg(e) {
-	console.log(e);
 	var src = e.src || e.target.src;
 	document.getElementById('img-container-inner').src = src;
 	ts('#modal').modal('show');
+}
+
+function resize() {
+	var list1 = document.getElementById("timetable");
+	var list2 = document.getElementById("other");
+	list1.classList.remove("one", "two", "three");
+	list2.classList.remove("one", "two", "three");
+	if (window.innerWidth < 576) {
+		list1.classList.add("one");
+		list2.classList.add("one");
+	} else if (window.innerWidth < 992) {
+		list1.classList.add("two");
+		list2.classList.add("two");
+	} else {
+		list1.classList.add("three");
+		list2.classList.add("three");
+	}
 }
